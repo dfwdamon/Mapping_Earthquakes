2e5c8b19030d2f37b84c4b7b01d6a05c3fd21273
Damon <damonjphillips@gmail.com>
2022-11-19 13:39:34 -0600

    challenge

---
+++ Earthquake_Challenge/static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZGZ3ZGFtb24iLCJhIjoiY2w5eGtmNDlzMGIzbDNvczVoZXRqcDNqeSJ9.-Poo3M3Wry7l9uSBB-0RhA";
+// pk.eyJ1IjoiZGZ3ZGFtb24iLCJhIjoiY2w5eGtmNDlzMGIzbDNvczVoZXRqcDNqeSJ9.-Poo3M3Wry7l9uSBB-0RhA
